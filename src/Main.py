9bed69645e6985dabd883c38789a2060f6193a80
Łukasz Fornalczyk <fornal96@o2.pl>
2014-12-09 00:49:10 +0100

    Poprawa maina

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,17 @@
-'''
-Created on 07-12-2014
-
-@author: Max
-'''
 from test.TestPyGame import TestPyGame
+from test.LabyrinthCreator import LabyrinthCreator
 
 if __name__ == '__main__':
-    testPyGame = TestPyGame();
-    testPyGame.showSomething();
-    testPyGame.getImagesNamesFromDirectory();
+    creator = LabyrinthCreator();
+    creator.initializeLabyrinth();
+    creator.createLabyrinthEntrance();
+    creator.createLabyrinthExit();
+    creator.showLab();
     
+    testPyGame = TestPyGame(creator.LABYRINTH_HEIGHT, creator.LABYRINTH_WIDTH);
+    testPyGame.showLabyrinth(creator.labyrinthArray, creator.LABYRINTH_HEIGHT, 
+                             creator.LABYRINTH_WIDTH, creator.LABYRINTH_ENTRANCE,
+                             creator.LABYRINTH_EXIT, creator.LABYRINTH_ROAD,
+                             creator.LABYRINTH_WALL, creator.xEntrancePosition, 
+                             creator.yEntrancePosition, creator.xExitPosition, 
+                             creator.yExitPosition);
