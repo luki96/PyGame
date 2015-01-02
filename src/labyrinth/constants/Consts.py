edfffc15eb4df102c1d761fd0588a112c4cbc24a
Łukasz Fornalczyk <fornal96@o2.pl>
2015-01-02 18:08:14 +0100

    Dodanie modulu przechowujacego wartosci stale

---
+++ src/labyrinth/constants/Consts.py
@@ -0,0 +1,17 @@
+class Consts(object):
+    
+    LABYRINTH_HEIGHT = 5;
+    LABYRINTH_WIDTH = 5;
+    LABYRINTH_WALL = 0;
+    LABYRINTH_ROAD = 1;
+    LABYRINTH_BEST_ROAD = 2
+    LABYRINTH_ENTRANCE = 3;
+    LABYRINTH_EXIT = 4;
+    LABYRINTH_ROAD_LEFT = 5;      'komora z nitka w lewo'
+    LABYRINTH_ROAD_RIGHT = 6;     'komora z nitka w prawo'
+    LABYRINTH_ROAD_UP = 7;        'komora z nitka w gore'
+    LABYRINTH_ROAD_DOWN = 8;      'komora z nitka w dol'
+    PROBABILITY = 0.5;
+    def __init__(self):
+        pass;
+        
